6a05d927069ba083502b86e217cf5dc155b9d81c
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-04-22 12:23:49 -0300

    Fix: adjust Team entity for validation

--- src/main/java/com/matheusmaciel/championship/entity/Team.java
+++ src/main/java/com/matheusmaciel/championship/entity/Team.java
@@ -1,10 +1,17 @@
 package com.matheusmaciel.championship.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Team {
 
     @Id
@@ -12,12 +19,16 @@ public class Team {
     private Integer id;
 
     @Column(nullable = false, length = 50)
+    @NotBlank(message = "Name is required")
     private String name;
 
-    @Column(length = 4)
+    @Column(length = 4, nullable = false)
     private String code;
 
     @Column(length = 2)
     private String state;
+
+    @Column(length = 50)
+    private String stadium;
 
 }
